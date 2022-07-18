64424cec62fba9b769494bc72bc6cff923b2504a
Lucas Reis <lucasreis_cod@hotmail.com>
2022-07-18 12:13:45 -0300

    Criando uma langind page com Html,CSS e JS

---
+++ assets/card.js
@@ -0,0 +1,17 @@
+const tenis1 = document.getElementById('tenis1')
+const tenis2 = document.getElementById('tenis2')
+
+const setaRight = document.getElementById('right')
+const setaLeft = document.getElementById('left')
+
+setaLeft.addEventListener('click', ()=>{
+    tenis1.style.left = '650px'
+    tenis2.style.left= '-50px'
+})
+
+setaRight.addEventListener('click', ()=>{
+    tenis1.style.left = '-50px'
+    tenis2.style.left = '-650px'
+})
+
+console.log('teste')
